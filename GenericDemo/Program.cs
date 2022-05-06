6e824b9a152364e23be45e356a8554c288fefbff
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-07 02:14:33 +0530

    Add project files.

---
+++ GenericDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    public class Program
+    {
+         static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+            char[] charArray = { 'A', 'N', 'I', 'K', 'E', 'T' };
+
+            //Program.toPrint(intArray);
+            //Program.toPrint(doubleArray);
+            //Program.toPrint(charArray);
+
+            //using Generic Method
+            Console.WriteLine("Using Generic Method");
+            sample.toPrint<int>(intArray);
+            sample.toPrint <double>(doubleArray);
+            sample.toPrint<char>(charArray);
+            //using generic class
+            Console.WriteLine("Using Generic Class");
+            new PrintArray<int>(intArray).ToPrint();
+            new PrintArray<double>(doubleArray).ToPrint();
+            new PrintArray<char>(charArray).ToPrint();
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ GenericDemo/sample.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    internal class sample
+    {
+        // Generic Method
+        public static void toPrint<T>(T[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            //for (int i = 0; i < inputArray.Length; i = i + 2)
+            //{
+            //    Console.WriteLine(inputArray[i]);
+            //}
+            Console.WriteLine("----------------------------------");
+        }
+
+    }
+
+    //Generic Class
+    public class PrintArray<T>
+    {
+        private T[] inputArray;
+        public PrintArray(T[] inputArray)
+        {
+            this.inputArray = inputArray;
+        }
+        public void ToPrint()
+        {
+            foreach (var element in inputArray) 
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-------------------------------------");
+        }
+    }
+}
+
+
+
+
